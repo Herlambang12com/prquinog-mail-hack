2e1c81463411b73fc6da30161693d0490b65da36
H.A.Kvortex <57755112+Herlambang12com@users.noreply.github.com>
2019-12-07 02:06:24 +0700

    Add files via upload

---
+++ pantat.py
@@ -0,0 +1,41 @@
+#mau ngapain haaa tolol bikin sendiri tolol
+#gw ksih virus kalo buka 
+#buka gih gw gak tanggung jawap 
+#hp lemot atau ke restrat
+import os 
+import random
+import sys
+import time
+os.system("clear")
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 1)
+#ubah angka 0.1 sesuai impian kamu
+#angka terkecil ialah yang paling cepat
+#angka terbesar ialah yang paling lambat
+print '     ____  ____  ____  __  _______   ______'
+print '    / __ \/ __ \/ __ \/ / / /  _/ | / / __ \ '
+print '   / /_/ / /_/ / / / / / / // //  |/ / / / / '
+print '  / ____/ _, _/ /_/ / /_/ // // /|  / /_/ /  '
+print ' /_/   /_/ |_|\___\_\____/___/_/ |_/\____/ '
+print ' [#########################################################################################]'
+print ' [#                     SELAMAT DATANG DI TEAM PRQUINO INDONESIA                         #]'
+print ' [#########################################################################################]'
+print ' [#git hub=https://github.com/Herlambang12com/                                           #]'
+print ' [#########################################################################################]'
+print ' [# grup whatsap=https://chat.whatsapp.com/BJCsL2CPca546fXA7n1d2r                        #]'
+print ' [#########################################################################################]'
+print ' #SELAMAT MENIKMATI# '
+time.sleep(6)
+mengetik('KALIAN LODING DULU\n \n> > > > > > > > > > > > > > >]100\n \nDI SINI SAYA AKAN MEMBAGIKAN BAGAIMANA CARA MENGINSTAL SEMUA TOOLS TERMUX  \n \n $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n 1.SIAPKAN SEBAGAI BERIKUT \n 1.internet \n 2.pcr kalo punya \n 3.APK TERMUX \n \n > > > > > > > > > > > > > > > > > > [100] ')
+print ' COPY LINK DI BAWAH '
+print ' http://192.168.0.2:8080 '
+print ' lalu buka browser '
+print ' paste '
+print ' SETELAH LOGIN '
+print ' KALIAN TINGGAL COPY DAH '
+print ' MASUK LALU INSTALL DI TERMUX KALIAN '
+print ' ASSALAMUALAIKUM '
